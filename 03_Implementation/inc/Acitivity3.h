431546b843c0df8dfdd109bbc7f966a98b5d76be
harshtheraj <81011974+harshtheraj@users.noreply.github.com>
2021-12-01 10:25:01 +0530

    Create Acitivity3.h

---
+++ 03_Implementation/inc/Acitivity3.h
@@ -0,0 +1,16 @@
+#ifndef _Activity3_h_
+#define _Activity3_h_
+
+/**
+ * @brief Initializes Timer0(8-bit) 
+ * 
+ */
+void Timer_init();
+
+/**
+ * @brief Activity-3 To generate PWM according to the data sensed by the ADC channel
+ * 
+ */
+void Activity3(int);
+
+#endif
