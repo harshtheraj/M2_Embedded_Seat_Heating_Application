e20063b5823e0710102515d299068bd6f18ad8a9
harshtheraj <81011974+harshtheraj@users.noreply.github.com>
2021-12-01 10:24:26 +0530

    Create Acitivity2.h

---
+++ 03_Implementation/inc/Acitivity2.h
@@ -0,0 +1,17 @@
+#ifndef _Activity2_h_
+#define _Activity2_h_
+
+/**
+ * @brief Initializes ADC Channels and its Registers
+ * 
+ */
+void ADC_init();
+
+/**
+ * @brief Activity-2 To take data from temperature sensor through ADC channel
+ * 
+ * @return int 
+ */
+int Activity2(char);
+
+#endif
